1b4689d26c1f190fc06d76d12246fb3693e3101a
Madandola Hammed <aramicrown@gmail.com>
2022-09-25 15:40:33 +0100

    style: prettier formating

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'doc', 'style', 'refactor', 'test', 'revert', 'build', 'ci', 'perf', 'conf']
-    ]
-  }
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			["feat", "fix", "doc", "style", "refactor", "test", "revert", "build", "ci", "perf", "conf"]
+		]
+	}
 };
--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,16 +1,16 @@
-import App from '@app/App';
-import { fireEvent, render, screen } from '@testing-library/react';
+import App from "@app/App";
+import { fireEvent, render, screen } from "@testing-library/react";
 
-test('count is 0 at first', () => {
-  render(<App />);
+test("count is 0 at first", () => {
+	render(<App />);
 
-  expect(screen.getByText('count is: 0')).toBeDefined();
+	expect(screen.getByText("count is: 0")).toBeDefined();
 });
 
-test('increments count', () => {
-  render(<App />);
+test("increments count", () => {
+	render(<App />);
 
-  fireEvent.click(screen.getByRole('button'));
+	fireEvent.click(screen.getByRole("button"));
 
-  expect(screen.getByText('count is: 1')).toBeDefined();
+	expect(screen.getByText("count is: 1")).toBeDefined();
 });
