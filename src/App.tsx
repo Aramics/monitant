9db5057376c5b82548c5f87dc4c2834bde648493
Madandola Hammed <aramicrown@gmail.com>
2022-09-25 18:49:23 +0100

    refactor: scaffolding away from starter

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = (): JSX.Element => {
+	return (
+		<main>
+			<header>Monitant</header>
+			<div>Balance monitor table</div>
+			<footer>Footer</footer>
+		</main>
+	);
+};
+
+export default App;
